6faf837fed81c1f9afe3ee879f490494758a7ad9
showeimer <showeimer@gmail.com>
2017-09-18 20:39:12 -0400

    redux boilerplate - created Provider and store in App.js, created boilerplate for reducers and integrated into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 class App extends Component {
   render() {
     return(
-      <View>
-        Hi
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          Hi
+        </View>
+      </Provider>
     )
   }
 }
