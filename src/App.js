9077dc89bdf1079110a7d7ed6d6bcae044d1749c
showeimer <showeimer@gmail.com>
2017-09-18 20:54:30 -0400

    had to reinstall npm packages, fixed syntax error in android and ios files, added Header to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return(
       <Provider store={createStore(reducers)}>
         <View>
-          Hi
+          <Header headerText="Tech Stack" />
         </View>
       </Provider>
     )
