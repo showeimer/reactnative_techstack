5b039d8aa8f733b8c110e08d282fa5eefe79a4be
showeimer <showeimer@gmail.com>
2017-09-18 23:07:46 -0400

    changed ListView to FlatList because of deprecation, converted code to FlatList boilerplate, list renders to app screen

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
 
-  componentDidMount() {
-    // boilerplate code for ListView
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-    this.dataSource = ds.cloneWithRows(this.props.libraries);
-  };
-
-  renderRow() {
-
-  };
+  renderItem({item}) {
+    return <ListItem library={item} />
+  }
 
   render () {
     return (
-      <ListView
-        dataSource={this.dataSource}
-        renderRow={this.renderRow}
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
       />
     )
   }
