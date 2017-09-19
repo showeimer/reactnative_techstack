242fff8a5b387b9a9540c1ce434297d280614549
showeimer <showeimer@gmail.com>
2017-09-18 20:30:15 -0400

    first commit, project boilerplate set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+
+class App extends Component {
+  render() {
+    return(
+      <View>
+        Hi
+      </View>
+    )
+  }
+}
+
+export default App;
